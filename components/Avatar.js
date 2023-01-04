148f8199b627c2bdc71cb7c39251f16b9a5607cc
AliMDhanani <alidhanani2000@gmail.com>
2023-01-04 12:15:23 +0000

    fixed avatar image bug

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,7 +3,7 @@ import React, { lazy } from "react";
 const Avatar = ({ url, className }) => {
   return (
     <img
-      src={url}
+      src="https://media.licdn.com/dms/image/C4E03AQGC-emMf82Vqw/profile-displayphoto-shrink_200_200/0/1619178667306?e=1678320000&v=beta&t=ZwUN_TcDdOdvyr8gzVB4_wPFkxP5wvQVbaSu8XJsX_c"
       alt="profile picture"
       className={`rounded-full h-10 cursor-pointer transition duration-150 transform hover:scale-110 ${className} `}
     />
--- components/Header.js
+++ components/Header.js
@@ -40,7 +40,7 @@ const Header = () => {
             Search
           </button>
         </form>
-        <Avatar className="ml-auto" url="https://media-exp1.licdn.com/dms/image/C4E03AQGC-emMf82Vqw/profile-displayphoto-shrink_200_200/0/1619178667306?e=1669248000&v=beta&t=_aUJIbLi0qPQOUR2OZsxr3kkbel6QeBdxQc8jzLfc8k" />
+        <Avatar className="ml-auto" />
       </div>
       <HeaderOptions />
     </header>
